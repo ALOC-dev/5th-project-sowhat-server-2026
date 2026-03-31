116e111eddd19f42fd9fdc8d10c3bf6e4b19d1c8
pushnu <kmumj0313@gmail.com>
2026-03-31 20:15:53 +0900

    feat: add SQLAlchemy models

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.users import UserInfo
+from app.models.articles import Article
+from app.models.common_analyses import CommonAnalysis
+from app.models.personal_analyses import PersonalAnalysis
+
+__all__ = [
+    "UserInfo",
+    "Article",
+    "CommonAnalysis",
+    "PersonalAnalysis",
+]
---
+++ app/models/articles.py
@@ -0,0 +1,21 @@
+from enum import Enum as PyEnum
+
+from sqlalchemy import Column, DateTime, Enum as SqlEnum, Integer, String, Text
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+
+class CategoryEnum(PyEnum):
+    POLITICS = "POLITICS"
+    ECONOMY = "ECONOMY"
+    SOCIETY = "SOCIETY"
+
+
+class Article(Base):
+    __tablename__ = "article"
+
+    article_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    date = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    content = Column(Text, nullable=False)
+    category = Column(SqlEnum(CategoryEnum, name="category_enum"), nullable=False)
