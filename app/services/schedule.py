edd5c9171e9d07021c2248fd152eae8679f101ab
Jiin <jxxniie05@gmail.com>
2026-08-28 06:40:19 +0900

    chore: '세계' 카테고리 임시 크롤링 중단

--- app/services/schedule.py
+++ app/services/schedule.py
@@ -31,7 +31,7 @@ YONHAP_RSS: dict[str, str] = {
     "연합뉴스(정치)": "https://www.yna.co.kr/rss/politics.xml",
     "연합뉴스(경제)": "https://www.yna.co.kr/rss/economy.xml",
     "연합뉴스(사회)": "https://www.yna.co.kr/rss/society.xml",
-    "연합뉴스(세계)": "https://www.yna.co.kr/rss/international.xml",
+    # "연합뉴스(세계)": "https://www.yna.co.kr/rss/international.xml",
     # "연합뉴스(문화)": "https://www.yna.co.kr/rss/culture.xml",
     # "연합뉴스(스포츠)": "https://www.yna.co.kr/rss/sports.xml",
     # "연합뉴스(연예)": "https://www.yna.co.kr/rss/entertainment.xml",
